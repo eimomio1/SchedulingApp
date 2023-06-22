6ddb6692fa15609981dad59507bb75f5503db8ca
malbeastin <79538386+malbeastin@users.noreply.github.com>
2023-06-21 21:53:36 -0400

    Update ViewBookedAppointments.java

--- ViewBookedAppointments.java
+++ ViewBookedAppointments.java
@@ -1,25 +1,48 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
-public class ViewBookedAppointments {
+
+import javafx.scene.control.ToggleGroup;
+
+
+public class ViewBookedAppointments implements Initializable {
 
 	
+
+    @FXML
+    public Button backtomenu;
+
+    @FXML
+    public ToggleGroup reportViewTgl;
 	
 	
-private Button backtomenu;	
+
 	
 
 public void backToMenu(ActionEvent event) throws IOException {
     Main m = new Main();
 
-    m.resizeScene("MainMenu.fxml");
+    m.changeScene("MainMenu.fxml");
 
+}
+
+
+
+
+
+@Override
+public void initialize(URL arg0, ResourceBundle arg1) {
+	
+	
+	
 }
 	
 	
