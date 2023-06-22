ad8d76a6cc7342c5da6fe863c847965410d1131b
malbeastin <79538386+malbeastin@users.noreply.github.com>
2023-06-21 22:01:13 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,8 @@
+module NewJavaFXProject {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires java.sql;
+	requires javafx.graphics;
+	opens application to javafx.graphics, javafx.fxml;
+}
