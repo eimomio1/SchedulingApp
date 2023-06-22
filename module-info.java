e17426081e9fd77a5838f30f26e2bfa1b9a58d05
malbeastin <79538386+malbeastin@users.noreply.github.com>
2023-06-22 16:49:04 -0400

    Delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module NewJavaFXProject {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires java.sql;
-	requires javafx.graphics;
-	opens application to javafx.graphics, javafx.fxml;
-}
