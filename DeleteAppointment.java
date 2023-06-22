9eb7d7eab16167f0f64ea7b5f5aad054a32d3766
malbeastin <79538386+malbeastin@users.noreply.github.com>
2023-06-21 21:47:38 -0400

    Update DeleteAppointment.java

--- DeleteAppointment.java
+++ DeleteAppointment.java
@@ -1,14 +1,12 @@
 package application;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,7 +25,7 @@ public class DeleteAppointment implements Initializable {
 	public void returnButtonMeth(ActionEvent apple) throws IOException {
 		
 		Main banana = new Main();
-		banana.resizeScene("MainMenu.fxml");
+		banana.changeScene("MainMenu.fxml");
 	}
 
 	@Override
