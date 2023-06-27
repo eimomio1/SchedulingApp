dc1cd9cdf106538bc4c1a29c7bbe776c8578e7b2
033-Coder <137132515+033-Coder@users.noreply.github.com>
2023-06-26 23:20:58 -0400

    Update create.sql

--- database/sql/create.sql
+++ database/sql/create.sql
@@ -5,10 +5,11 @@ CREATE TABLE IF NOT EXISTS public.appointments
     id serial NOT NULL,
     description character varying(200),
     location character varying(200),
-    type character varying(200),
+    title character varying(200),
     date character varying(200),
     starttime character varying(200),
     endtime character varying(200),
+    name character varying(200),
     PRIMARY KEY (id)
 );
 
