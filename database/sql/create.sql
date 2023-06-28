5224f2d031a221f108d0799e0078b989fbe29e5a
033-Coder <137132515+033-Coder@users.noreply.github.com>
2023-06-28 10:07:54 -0400

    unique combination of starttime and endtime

--- database/sql/create.sql
+++ database/sql/create.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS public.appointments
     starttime character varying(200),
     endtime character varying(200),
     name character varying(200),
+    UNIQUE (starttime, endtime),
     PRIMARY KEY (id)
 );
 
