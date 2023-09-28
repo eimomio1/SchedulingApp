390e4d0e6c63d0b18e6666a5d9a6021f5b09f767
eimomio1 <79538386+eimomio1@users.noreply.github.com>
2023-09-28 04:02:33 -0400

    Add files via upload

---
+++ application/database/sql/create.sql
@@ -0,0 +1,36 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS public.appointments
+(
+    id serial NOT NULL,
+    description character varying(200),
+    location character varying(200),
+    title character varying(200),
+    date character varying(200),
+    starttime character varying(200),
+    endtime character varying(200),
+    name character varying(200),
+    UNIQUE (name, starttime, endtime),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.records
+(
+    username character varying(200),
+    password character varying(200)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.user_appointments
+(
+    user_id bigint NOT NULL,
+    name character varying(200) NOT NULL,
+    email character varying(200)
+);
+
+END;
+
+
+
+
+END;
